7b63880fa22ca7bbe74d0db6f7efe44fc468045c
evgenii_kotsechko <kotsechkog@gmail.com>
2018-05-23 15:51:25 +0300

    asd

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,75 @@
 const App = {
   init() {
+    let counterSatisfied = +($('#satisfied').text());
+    
+    $.fn.scrollView = function () {
+       return this.each(function () {
+        $('html, body').animate({
+        scrollTop: $(this).offset().top
+        }, 500);
+       });
+    };
     
     this.handlers();
   },
 
   handlers() {
+
+    $('.scroll-to').click(function(e) {
+      e.preventDefault();
+      $('#sticky-header').scrollView();
+    });
+
+    $('#scroll-top').click(function(e) {
+      e.preventDefault();
+      $('#sticky-header').scrollView();
+    });
+
+
+
     $('div.swiper-container').click( function (e) {
       App.getLike(e);
     });
+
+
     $('#burger').click( function (e) {
       e.preventDefault();
+      if ( +($(window).scrollTop()) < +($('#sticky-header').offset().top) ) {
+        $('#sticky-header').scrollView();
+      } 
       $('#burger').toggleClass('active');
-      $('#nav').toggleClass('active');});
+      $('#nav').toggleClass('active');
+    });
+
+
 
     $('#icon-search').click( function () {
       $('#search').toggleClass('active');});
+
+//     ( async function getSatisfied(){
+//       let randomTime = Math.random()*2000 + 500;
+//       if ( counterSatisfied <= 999) {
+//         setTimeout( function(){
+//        counterSatisfied += 1;
+//        getSatisfied();
+//       }, randomTime);
+//       }
+   
+// })();
+
     var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 'auto',
+      slidesPerView: 3,
       spaceBetween: 30,
+      breakpoints: {
+    1100: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    730: {
+      slidesPerView: 1
+    }
+  },
+
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -55,6 +107,7 @@ const App = {
       },
     });
   },
+
   getLike(e) {
     if (e.target.className == 'icon-heart') {
       e.preventDefault();
